b666732a35ac6e1c00604dd8fd56f9ee83cd1b8e
Emy Alwin <emy.alwin@gmail.com>
2024-02-19 12:48:48 +0100

    Initial Commit

---
+++ WeatherAppSwift1/WeatherAppSwift1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwift1App.swift
+//  WeatherAppSwift1
+//
+//  Created by Emy Alwin on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
